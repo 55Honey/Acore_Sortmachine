6b6d93558137a8977fbb00f6a4e2a95232427d95
55Honey <71938210+55Honey@users.noreply.github.com>
2021-01-18 22:54:04 +0100

    ElunaLua Version creation
    Added the initial .sortguid script to run along with ElunaLua in the core.

---
+++ AzerothcoreSortGuid.lua
@@ -0,0 +1,138 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Silvia
+-- Date: 10/01/2021
+-- Time: 18:12
+-- To change this template use File | Settings | File Templates.
+-- Originally created by Honey for Azerothcore
+-- requires ElunaLua module
+
+
+
+-- This script is supposed to alter your unique guid's in item_instance and their references in character_inventory, guild_bank_item and possibly customs
+-- to start from 1, counting up without gaps. Do not run this script/command while players are active.
+
+ConsoleOnly = true								-- if true the .sortguid command will not work ingame
+MinGMRank = 4									-- staff must have at least this rank to use .sortguid
+PrintProgress = 1000							-- how often the console or chat should print progression. 1000 means every 1000th item
+ChangeCustom = false							-- Is there a custom table e.g. from a transmog module to sort as well?
+CustomTableName = "Insert_table_name_here"		-- make this your custom tables name
+CustomColumnName = "Insert_column_name_here"	-- make this your custom tables column with the guid to change
+
+
+------------------------------------------
+-- NO ADJUSTMENTS REQUIRED BELOW THIS LINE
+------------------------------------------
+
+local PLAYER_EVENT_ON_LOGIN = 3
+local PLAYER_EVENT_ON_COMMAND = 42
+
+local function OnLogin(event, player)
+    if player:GetGMRank() >= MinGMRank and not ConsoleOnly then
+		player:SendBroadcastMessage("Command .sortguid active.")
+	end	
+end
+
+local function Sortguid(event, player, command)
+	if ConsoleOnly and player then return end					-- this script works only directly from the console if ConsoleOnly is set
+	if player then
+		if player:GetGMRank() < MinGMRank then return end		-- make sure the staff is properly ranked
+	end	
+	
+    local SortCounter = 1
+
+    if command == "sortguid" then
+        QueryItemInstance(player)                      			--get Data from the DB, pass it to itemsGuidArrayLUA[row]
+        repeat
+		
+			if SortCounter == itemsGuidArrayLUA[SortCounter] then	-- if the line is already in the right place dont bother writing again
+				goto skip
+			end	
+			
+            -- Sort item guids
+			CharDBExecute("UPDATE item_instance SET guid="..SortCounter.." WHERE guid="..itemsGuidArrayLUA[SortCounter])
+            
+			-- adjust item references in player inventory
+			CharDBExecute("UPDATE character_inventory SET item="..SortCounter.." WHERE item="..itemsGuidArrayLUA[SortCounter])
+			
+			-- adjust item references in guild banks
+			CharDBExecute("UPDATE guild_bank_item SET item_guid="..SortCounter.." WHERE item_guid="..itemsGuidArrayLUA[SortCounter])
+			
+			-- adjust bag references in player inventory, if the item is a bag
+			if has_value(listOfBags, itemsGuidArrayLUA[SortCounter]) then
+				CharDBExecute("UPDATE character_inventory SET bag="..SortCounter.." WHERE bag="..itemsGuidArrayLUA[SortCounter])
+			end
+			
+			if ChangeCustom == true then
+				CharDBExecute("UPDATE "..CustomTableName.." SET "CustomColumnName.."="..SortCounter.." WHERE "..CustomColumnName.."="..itemsGuidArrayLUA[SortCounter])
+			end
+			
+            if player then
+				if ToInteger(SortCounter / PrintProgress) == tonumber(SortCounter / PrintProgress) then
+					player:SendBroadcastMessage("Progressing guid: "..SortCounter)
+				end	
+			else
+				if ToInteger(SortCounter / PrintProgress) == tonumber(SortCounter / PrintProgress) then
+					print("Progressing guid: "..SortCounter)
+				end	
+			end
+			::skip::
+			SortCounter = SortCounter + 1
+        until SortCounter == ItemCounter
+    end
+	itemsGuidArrayLUA = nil			-- free memory
+	print("Script .sortguid is done!")
+	return false
+end
+
+function QueryItemInstance(player)	--get Data from the DB, pass it to Lua arrays
+    ItemCounter = 1
+	itemsGuidArrayLUA = {}
+    --get the item_instance guid column
+	print("Reading items from DB...")
+	local itemsArraySQL = CharDBQuery("SELECT guid FROM item_instance")
+	print("Sorting items in an array...")
+	if itemsArraySQL then
+        repeat
+            itemsGuidArrayLUA[ItemCounter] = itemsArraySQL:GetUInt32(0)
+			--print("Reading item"..ItemCounter..": "..itemsGuidArrayLUA[ItemCounter])
+            ItemCounter = ItemCounter + 1
+        until not itemsArraySQL:NextRow()
+    end
+	itemsArraySQL = nil		-- free memory
+	
+	--get the character_inventory tables bag column
+	local n = 1
+	characterBagArrayLUA = {}	
+	listOfBags = {}
+	print("Reading bags from DB...")
+	local itemsArraySQL = CharDBQuery("SELECT bag FROM character_inventory")
+	print("Making a list of all bags guid's...")
+	if itemsArraySQL then
+        repeat
+            characterBagArrayLUA[n] = itemsArraySQL:GetUInt32(0)
+			--print("Reading bag"..n..": "..itemsGuidArrayLUA[n])
+			if characterBagArrayLUA[n] ~= 0 and not has_value(listOfBags, characterBagArrayLUA[n]) then
+				table.insert (listOfBags, characterBagArrayLUA[n])				-- make a list of all bags item guids
+			end
+            n = n + 1
+        until not itemsArraySQL:NextRow()
+    end
+	itemsArraySQL = nil		-- free memory
+end
+
+function ToInteger(number)
+    return math.floor(tonumber(number) or error("Could not cast '" .. tostring(number) .. "' to number.'"))
+end
+
+function has_value (tab, val)
+    for index, value in ipairs(tab) do
+        if value == val then
+            return true
+        end
+    end
+    return false
+end
+
+RegisterPlayerEvent(PLAYER_EVENT_ON_LOGIN, OnLogin)
+RegisterPlayerEvent(PLAYER_EVENT_ON_COMMAND, Sortguid)
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Acore_Sortmachine
+
+**This is total BETA. Make sure you have decent backups! Do not run it on a live server**
+
+This script is supposed to alter your unique guid's in item_instance and their references in character_inventory, guild_bank_item and possibly customs.
+It's purpose is to close gaps in case the servers #item_guid closes in on 4b.
+
+Running it repeatedly will skip executing SQL commands on already sorted items, until a single gap is occured.
+
+
+## Requirements:
+
+Compile your [Azerothcore](https://github.com/azerothcore/azerothcore-wotlk) with [Eluna Lua](https://www.azerothcore.org/catalogue-details.html?id=131435473).
+Add this script to your ../bin/release/lua_scripts/ directory.
+
+##Usage:
+
+- Run the worldserver standalone. Do **not** have players on the server. Do not have anything active which could potentially create items.
+- Type `.sortguid` into the console (recommended) or optionaly on an ingame client with sufficient rights.
+- Bring time. Local tests on a modern i9 with ssd ran for a split of a second with 1000guids while running this on a remote vps with hdd took 8 hours for ~1mil guids
+
+##Settings in the .lua file:
+
+ConsoleOnly = true
+If this is true the .sortguid command will not work ingame but only from the world server console
+
+MinGMRank = 4
+Staff must have at least this rank to use `.sortguid`
+
+PrintProgress = 1000
+How often the console or chat should print progression. 1000 means every 1000th item. Skipped or written doesnt matter.
+
+ChangeCustom = false
+If there is a custom table e.g. from a transmog module this must be set to true or all affected items will be lost/bugged
+
+CustomTableName = "Insert_table_name_here"
+Make this your custom tables name.
+
+CustomColumnName = "Insert_column_name_here"
+Make this your custom tables column with the items guid to change.
